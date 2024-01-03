26513952572d076b681b1c0b821148a3eca8e098
Robert Edwardes <rdedwardes@gmail.com>
2024-01-03 10:41:55 -0500

    backend completed

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    if request.methods == 'POST':
-        return 
+    if request.method == 'POST':
+        address = request.form['address']
+        CENSUS_API = f'https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address={address}&benchmark=2020&format=json'
+        census_json = requests.get(CENSUS_API)
+        if census_json.status_code != 200:
+            return f'<h1>JSON request went wrong got {census_json.status_code}</h1>'
+        return census_json.json()
     else:
         return render_template('index.html')
 
