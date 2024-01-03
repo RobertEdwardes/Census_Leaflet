b01244342e8ddebb1b8e9df7c4eb2adee3684724
Robert Edwardes <rdedwardes@gmail.com>
2024-01-03 12:48:41 -0500

    simple frontend done

--- app.py
+++ app.py
@@ -9,9 +9,14 @@ def index():
         address = request.form['address']
         CENSUS_API = f'https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address={address}&benchmark=2020&format=json'
         census_json = requests.get(CENSUS_API)
+        data=census_json.json()
+        addressMatches = data['result']['addressMatches']
+        if len(addressMatches) == 0:
+            return f'<h1> No matches for input {address} </h1>'
+        addressMatches = addressMatches[0]
         if census_json.status_code != 200:
-            return f'<h1>JSON request went wrong got {census_json.status_code}</h1>'
-        return census_json.json()
+            return f'<h1>JSON request went wrong <br> got {census_json.status_code}</h1>'
+        return render_template('index.html', data=addressMatches)
     else:
         return render_template('index.html')
 
